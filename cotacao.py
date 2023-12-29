79b5401db913e9ea0f026b0c7e2c1477e6cc357f
ederpferreira <eder.ferreira.ads@gmail.com>
2023-12-29 17:28:20 -0400

    - criação da janela

--- cotacao.py
+++ cotacao.py
@@ -20,7 +20,26 @@ def cotacoes():
     BTC  : {cotacao_btc}
     Data : {formato_dt}'''
     print(texto)
-    
 
-cotacoes()
+     # Muda a cor do texto em result_cotacoes
+    result_cotacoes.config(text=texto, fg="green")  # Por exemplo, verde
+ 
+janela = Tk()
+janela.title("Cotação Moedas")
+janela.geometry("250x200")
 
+texto= Label(janela, text="Buscar Cotações Dollar/Euro/ BTC")
+texto.grid(column=1, row=0, padx=15, pady=5)
+
+texto2 = Label(janela, text="Clique para buscar!")
+texto2.grid(column=1, row=1, padx=5, pady=5)
+
+botao = Button(janela, text="Atualizar", command=cotacoes)
+botao.grid(column=1, row=2)
+
+# Trazer o texto com as cotações na janela
+result_cotacoes = Label(janela, text="")
+result_cotacoes.grid(column=1, row=3, padx=5, pady=5)
+
+# Inicializa a função de cotações
+janela.mainloop()
